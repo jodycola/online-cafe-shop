7913b40ab9d05f0290d27aba474e6012ffdaba0c
Kody Samaroo <kody.samaroo@gmail.com>
2023-04-12 23:36:51 -0400

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Container = styled.div`
     height: 60px;
-    background-color: blue;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.div`
+    cursor: pointer;
+`;
+const Search = styled.div`
+    border: .5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.div`
 `
+
+
+
+const Center = styled.div`
+    flex: 1;
+`;
+
+
+
+const Right = styled.div`
+    flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-        Navbar
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <Search>
+                    input
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </Search>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
     </Container>
   )
 }
