5851c6f85f73f897fcc5d779498eae66f7b71ec9
Kody Samaroo <kody.samaroo@gmail.com>
2023-05-23 20:31:11 -0400

    commit before rework

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { menuItems } from '../data';
 import ProductItem from './ProductItem';
+import axios from 'axios';
 
 const Product = ({ category, filter, sort }) => {
+
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/api/products")
+        console.log(res)
+      } catch(err) {}
+    };
+    getProducts()
+  }, [category])
 
     const displayMenu = menuItems.map((product) => {
         return <ProductItem key={product.id} product={product}/>
